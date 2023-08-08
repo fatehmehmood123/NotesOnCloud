6f349f5954759937e476d2b9d811ce03b68f3d1e
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-08-08 20:36:50 +0500

    layout of notes changed to vertical

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ function showNotes() {
     let html = "";
     data.forEach(function (element, index) {
         html += `
-        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+        <div class="noteCard my-2 mx-2 card" >
         <div class="card-body">
         <h5 class="card-title"> ${element.title===""?"No Title":element.title}</h5>
         <p  class="card-text">${element.body.replaceAll("\n","<br>")} </p>
