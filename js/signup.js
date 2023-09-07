cac43f633500cb4320607b2fddf7029f6995c4bf
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-09-07 20:01:21 +0500

    email verification logic applied

--- js/signup.js
+++ js/signup.js
@@ -1,54 +1,96 @@
+// import cryptoRandomString from "crypto-random-string";
+
 let addEmail = document.getElementById("addEmail");
 let addPassword = document.getElementById("password-input");
 let addConfirmPassword = document.getElementById("addConfirmPassword");
 let signupBtn = document.getElementById("signupBtn");
 function checkPasswordMatch() {
-  if (addPassword.value === addConfirmPassword.value && addPassword.value !== "") {
+  if (
+    addPassword.value === addConfirmPassword.value &&
+    addPassword.value !== ""
+  ) {
     signupBtn.classList.remove("disabled");
-  }else{
+  } else {
     signupBtn.classList.add("disabled");
-    
   }
 }
 addPassword.addEventListener("input", checkPasswordMatch);
 addConfirmPassword.addEventListener("input", checkPasswordMatch);
-
-
-signupBtn.addEventListener("click", (e)=>{
-     // Create an object with the book data
+function generateRandomCode() {
+  const min = 100000; // Minimum value (inclusive)
+  const max = 999999; // Maximum value (inclusive)
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+signupBtn.addEventListener("click", (e) => {
+  // Create an object with the user data
+  // const token = cryptoRandomString({ length: 6, type: "numeric" });
+  
+  const token = generateRandomCode();
+  let verifyEmail = addEmail.value;
+  const isVerified = false;
+  console.log(token);
   let userData = {
     email: addEmail.value,
-    password: addPassword.value
+    password: addPassword.value,
+    token: token,
+    isVerified:isVerified
   };
   // Send the POST request to the server
-  fetch('https://notesbackend-ten.vercel.app/register', {
-    method: 'POST',
+  fetch("https://notesbackend-ten.vercel.app/register", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(userData)
+    body: JSON.stringify(userData),
   })
-  .then(response => {
-    if (response.ok) {
-      // Book added successfully
+    .then((response) => {
+      if (response.ok) {
+        // window.location.href = "login.html";
+        // window.location.href = `verify.html?email=${encodeURIComponent(
+        //   verifyEmail
+        // )}`;
+        // Perform any other necessary actions after successful addition
+        return response.json();
+      } else {
+        // Error occurred during addition
+        console.error("Error adding User:", response.statusText);
+      }
+    })
+    .then((data) => {
+      // Send a verification email to the user's email address
+      sendVerificationEmail(data.email, token);
      
-      window.location.href = "login.html";
-      // Perform any other necessary actions after successful addition
-    } else {
-      // Error occurred during addition
-      console.error('Error adding User:', response.statusText);
-    }
-  })
-  .catch(error => {
-    // Network error occurred
-    console.error('Network error:', error);
-  });
-    e.preventDefault();
-    addEmail.value = "";
-    addPassword.value = "";
-    addConfirmPassword.value = "";
+      // Redirect to the verification page with email as a query parameter
+      window.location.href = `verify.html?email=${data.email}`;
+    })
+    .catch((error) => {
+      // Network error occurred
+      console.error("Network error:", error);
+    });
+  e.preventDefault();
+  addEmail.value = "";
+  addPassword.value = "";
+  addConfirmPassword.value = "";
 });
 
+function sendVerificationEmail(email, verificationToken) {
+  // Use the Fetch API or an email library to send an email with the verificationToken to the provided email address
+  fetch('https://notesbackend-ten.vercel.app/sendVerificationEmail', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, verificationToken }),
+  })
+  .then(response => {
+      if (!response.ok) {
+          console.error('Error sending verification email:', response.statusText);
+      }
+  })
+  .catch(error => {
+      console.error('Email sending error:', error);
+  });
+}
 function togglePasswordVisibility(elementId) {
   const passwordField = document.getElementById(elementId);
   if (passwordField.type === "password") {
@@ -73,96 +115,101 @@ addEventListener("DOMContentLoaded", (event) => {
   requirements.forEach((element) => element.classList.add("wrong"));
 
   password.addEventListener("focus", () => {
-      passwordAlert.classList.remove("d-none");
-      if (!password.classList.contains("is-valid")) {
-          password.classList.add("is-invalid");
-      }
+    passwordAlert.classList.remove("d-none");
+    if (!password.classList.contains("is-valid")) {
+      password.classList.add("is-invalid");
+    }
   });
 
   password.addEventListener("input", () => {
-      let value = password.value;
-      if (value.length < 8) {
-          lengBoolean = false;
-      } else if (value.length > 7) {
-          lengBoolean = true;
-      }
+    let value = password.value;
+    if (value.length < 8) {
+      lengBoolean = false;
+    } else if (value.length > 7) {
+      lengBoolean = true;
+    }
 
-      if (value.toLowerCase() == value) {
-          bigLetterBoolean = false;
+    if (value.toLowerCase() == value) {
+      bigLetterBoolean = false;
+    } else {
+      bigLetterBoolean = true;
+    }
+
+    numBoolean = false;
+    for (let i = 0; i < value.length; i++) {
+      for (let j = 0; j < numbers.length; j++) {
+        if (value[i] == numbers[j]) {
+          numBoolean = true;
+        }
+      }
+    }
+
+    specialCharBoolean = false;
+    for (let i = 0; i < value.length; i++) {
+      for (let j = 0; j < specialChars.length; j++) {
+        if (value[i] == specialChars[j]) {
+          specialCharBoolean = true;
+        }
+      }
+    }
+
+    if (
+      lengBoolean == true &&
+      bigLetterBoolean == true &&
+      numBoolean == true &&
+      specialCharBoolean == true
+    ) {
+      password.classList.remove("is-invalid");
+      password.classList.add("is-valid");
+
+      requirements.forEach((element) => {
+        element.classList.remove("wrong");
+        element.classList.add("good");
+      });
+      passwordAlert.classList.remove("alert-warning");
+      passwordAlert.classList.add("alert-success");
+    } else {
+      password.classList.remove("is-valid");
+      password.classList.add("is-invalid");
+
+      passwordAlert.classList.add("alert-warning");
+      passwordAlert.classList.remove("alert-success");
+
+      if (lengBoolean == false) {
+        leng.classList.add("wrong");
+        leng.classList.remove("good");
       } else {
-          bigLetterBoolean = true;
+        leng.classList.add("good");
+        leng.classList.remove("wrong");
       }
 
-      numBoolean = false;
-      for (let i = 0; i < value.length; i++) {
-          for (let j = 0; j < numbers.length; j++) {
-              if (value[i] == numbers[j]) {
-                  numBoolean = true;
-              }
-          }
-      }
-
-      specialCharBoolean = false;
-      for (let i = 0; i < value.length; i++) {
-          for (let j = 0; j < specialChars.length; j++) {
-              if (value[i] == specialChars[j]) {
-                  specialCharBoolean = true;
-              }
-          }
-      }
-
-      if (lengBoolean == true && bigLetterBoolean == true && numBoolean == true && specialCharBoolean == true) {
-          password.classList.remove("is-invalid");
-          password.classList.add("is-valid");
-
-          requirements.forEach((element) => {
-              element.classList.remove("wrong");
-              element.classList.add("good");
-          });
-          passwordAlert.classList.remove("alert-warning");
-          passwordAlert.classList.add("alert-success");
+      if (bigLetterBoolean == false) {
+        bigLetter.classList.add("wrong");
+        bigLetter.classList.remove("good");
       } else {
-          password.classList.remove("is-valid");
-          password.classList.add("is-invalid");
-
-          passwordAlert.classList.add("alert-warning");
-          passwordAlert.classList.remove("alert-success");
-
-          if (lengBoolean == false) {
-              leng.classList.add("wrong");
-              leng.classList.remove("good");
-          } else {
-              leng.classList.add("good");
-              leng.classList.remove("wrong");
-          }
-
-          if (bigLetterBoolean == false) {
-              bigLetter.classList.add("wrong");
-              bigLetter.classList.remove("good");
-          } else {
-              bigLetter.classList.add("good");
-              bigLetter.classList.remove("wrong");
-          }
-
-          if (numBoolean == false) {
-              num.classList.add("wrong");
-              num.classList.remove("good");
-          } else {
-              num.classList.add("good");
-              num.classList.remove("wrong");
-          }
-
-          if (specialCharBoolean == false) {
-              specialChar.classList.add("wrong");
-              specialChar.classList.remove("good");
-          } else {
-              specialChar.classList.add("good");
-              specialChar.classList.remove("wrong");
-          }
+        bigLetter.classList.add("good");
+        bigLetter.classList.remove("wrong");
       }
+
+      if (numBoolean == false) {
+        num.classList.add("wrong");
+        num.classList.remove("good");
+      } else {
+        num.classList.add("good");
+        num.classList.remove("wrong");
+      }
+
+      if (specialCharBoolean == false) {
+        specialChar.classList.add("wrong");
+        specialChar.classList.remove("good");
+      } else {
+        specialChar.classList.add("good");
+        specialChar.classList.remove("wrong");
+      }
+    }
   });
 
   password.addEventListener("blur", () => {
-      passwordAlert.classList.add("d-none");
+    passwordAlert.classList.add("d-none");
   });
 });
